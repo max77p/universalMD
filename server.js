18a9ba7c155e2fa6163f638d8c294b5de5e6b2c9
max77p <shanindrakumar@gmail.com>
2018-07-28 11:37:51 -0400

    initial files

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser=require("body-parser");
+
+var app=express();
+var PORT = process.env.Port || 8080;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//set handlebars
+var exphbs=require("express-handlebars");
+app.engine("handlebars",exphbs({defaultLayout:"main"}));
+app.set("view engine","handlebars");
+
+var routes =require("./controllers/mdController.js");
+app.use(routes);
+
+app.listen(PORT,function(){
+    console.log(`App listening on PORT ${PORT}`);
+})
