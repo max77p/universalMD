4e5dae7b72d8630ca2c6860a8c91ad368a1da7c4
max77p <shanindrakumar@gmail.com>
2018-08-01 18:01:07 -0400

    updated views

--- controllers/mdController.js
+++ controllers/mdController.js
@@ -2,14 +2,14 @@ var express=require("express");
 var router=express.Router();
 var umd = require("../models/umdModel.js");
 
-router.get("/",function(req,res){
+router.get("/index",function(req,res){
     umd.all(function(data){
         console.log(data);
         var hasObject = {
             doctors: JSON.stringify(data)
           };
           console.log(hasObject);
-          res.render("index", hasObject);
+          res.render("index");
     });
 });
 
