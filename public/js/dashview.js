ac46835026800a27c072e42905fb925ee9b19e49
max77p <shanindrakumar@gmail.com>
2018-08-07 22:23:47 -0400

    updated this on button

--- public/js/dashview.js
+++ public/js/dashview.js
@@ -3,8 +3,11 @@ $('.individDoctor').hide();
 
 $('.nav-item').on("click", function () {
    var y=$(this).data("name");
+   var z=$(this);
+   $(this).find('a').addClass("active");
+    $(".nav-item").not(this).find('a').removeClass("active");
    $('.individDoctor').each(function(){
-        if($(this).data("name")===y){
+        if($(this).data("name")===y){//compare individ data name to button data name if equal then show doctors
             $(this).show();
         }
         else{
